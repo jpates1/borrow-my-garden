8e37788e584a814e8210f8954a79c7c281da26df
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-04-25 17:25:03 +0100

    Project setup

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require("mongodb");
+// const ATLAS_URI="mongodb+srv://TeamAllotment:qcLX25ZQRPwNTWr8@borrow-my-garden.6typkvu.mongodb.net/borrow-my-garden?retryWrites=true&w=majority";
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
@@ -6,16 +7,17 @@ const client = new MongoClient(Db, {
 });
  
 var _db;
- 
+
 module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
       // Verify we got a good "db" object
+      
       if (db)
       {
         _db = db.db("borrow-my-garden");
         console.log("Successfully connected to MongoDB."); 
-      }
+      } 
       return callback(err);
          });
   },
@@ -24,3 +26,18 @@ module.exports = {
     return _db;
   },
 };
+
+/* const { MongoClient } = require("mongodb"); 
+const Db = process.env.ATLAS_URI; 
+const client = new MongoClient(Db, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+ }); 
+  
+  let _db; 
+  
+  module.exports = { 
+    connectToServer: function (callback) { 
+      client.connect(function (err, db) { 
+        if (err) { 
+          console.error(err); return callback(err); } _db = db.db("borrow-my-garden"); console.log("Successfully connected to MongoDB."); return callback(); }); }, getDb: function () { return _db; }, }; */
--- server/server.js
+++ server/server.js
@@ -17,4 +17,3 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
-
