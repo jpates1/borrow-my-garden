0b180e427dade43613e0ab77de873118926d612f
jpates1 <jamesandrewpates@gmail.com>
2023-04-26 12:11:39 +0100

    adding server.js

--- server/server.js
+++ server/server.js
@@ -4,9 +4,40 @@ const cors = require("cors");
 const mongoose = require('mongoose');
 require("dotenv").config();
 const port = process.env.PORT || 2000;
+const JWT = require("jsonwebtoken");
+
+const gardensRouter = require("./routes/gardens");
+const tokensRouter = require("./routes/tokens");
+const usersRouter = require("./routes/users");
+
 
 app.use(cors());
 app.use(express.json());
+
+const tokenChecker = (req, res, next) => {
+
+  let token;
+  const authHeader = req.get("Authorization")
+
+  if(authHeader) {
+    token = authHeader.slice(7)
+  }
+
+  JWT.verify(token, process.env.JWT_SECRET, (err, payload) => {
+    if(err) {
+      console.log(err)
+      res.status(401).json({message: "auth error"});
+    } else {
+      req.user_id = payload.user_id;
+      next();
+    }
+  });
+};
+
+app.use("/gardens", tokenChecker, gardensRouter);
+app.use("/tokens", tokensRouter);
+app.use("/users", usersRouter);
+
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
