67330ce3d12342e2e865e6c0eb1c59b547ce1569
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-04-26 17:58:31 +0100

    Tutorial followed

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,53 @@
 import React from "react";
-// import { ChakraProvider } from '@chakra-ui/react';
  
 // We use Route in order to define the different routes of our application
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
  
 // We import all the components we need in our app
-//import Navbar from "./components/navbar";
-import SignUp from "./components/signup/SignUp";
+//import Navbar from "./components/navbar";import RecordList from "./components/recordList";
 //import Edit from "./components/edit";
-//import Create from "./components/create";
+import Create from "./components/signup/Create";
  
 const App = () => {
-  const navigate = useNavigate();
  return (
-  // <ChakraProvider>
    <div>
+     {/* <Navbar /> */}
      <Routes>
-       <Route path="/signup" element={<SignUp navigate={navigate} />} />
-      </Routes>
+       {/* <Route exact path="/" element={<RecordList />} />
+       <Route path="/edit/:id" element={<Edit />} /> */}
+       <Route path="/create" element={<Create />} />
+     </Routes>
    </div>
-  // </ChakraProvider>
  );
 };
  
 export default App;
+
+
+
+// import React from "react";
+// // import { ChakraProvider } from '@chakra-ui/react';
+ 
+// // We use Route in order to define the different routes of our application
+// import { Route, Routes, useNavigate } from "react-router-dom";
+ 
+// // We import all the components we need in our app
+// //import Navbar from "./components/navbar";
+// import SignUp from "./components/signup/SignUp";
+// //import Edit from "./components/edit";
+// //import Create from "./components/create";
+ 
+// const App = () => {
+//   const navigate = useNavigate();
+//  return (
+//   // <ChakraProvider>
+//    <div>
+//      <Routes>
+//        <Route path="/signup" element={<SignUp navigate={navigate} />} />
+//       </Routes>
+//    </div>
+//   // </ChakraProvider>
+//  );
+// };
+ 
+// export default App;
--- server/server.js
+++ server/server.js
@@ -1,65 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require('mongoose');
-require("dotenv").config();
+require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 2000;
-const JWT = require("jsonwebtoken");
-
-const gardensRouter = require("./routes/gardens");
-const tokensRouter = require("./routes/tokens");
-const usersRouter = require("./routes/users");
-
-
 app.use(cors());
 app.use(express.json());
-
-const tokenChecker = (req, res, next) => {
-
-  let token;
-  const authHeader = req.get("Authorization")
-
-  if(authHeader) {
-    token = authHeader.slice(7)
-  }
-
-  JWT.verify(token, process.env.JWT_SECRET, (err, payload) => {
-    if(err) {
-      console.log(err)
-      res.status(401).json({message: "auth error"});
-    } else {
-      req.user_id = payload.user_id;
-      next();
-    }
-  });
-};
-
-app.use("/gardens", tokenChecker, gardensRouter);
-app.use("/tokens", tokensRouter);
-app.use("/users", usersRouter);
-
-
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established succesfully");
-
-  // Get list of collection names in database
-  mongoose.connection.db.collections((err, collections) => {
-    if (err) throw err;
-    console.log("Collections:");
-    console.log(collections.map(c => c.s.name));
-  });
-  
-  
-});
-
+app.use(require("./routes/user"));
+// get driver connection
+const dbo = require("./db/conn");
+ 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
 });
-
