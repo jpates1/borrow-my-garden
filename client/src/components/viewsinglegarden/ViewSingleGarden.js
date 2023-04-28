fa68fa4ba8af7ac25fc3c58758c5dc4f8fed08e9
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-04-28 12:22:07 +0100

    Committing before pulling token fixes to main

--- client/src/components/viewsinglegarden/ViewSingleGarden.js
+++ client/src/components/viewsinglegarden/ViewSingleGarden.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ChakraProvider, Container, Button, Heading, Divider, Input, Select } from "@chakra-ui/react";
+import { ChakraProvider, Container, Button, Heading, Divider, Input, Select, Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
 
 const ViewSingleGarden = () => {
   return (
@@ -8,8 +8,21 @@ const ViewSingleGarden = () => {
       <Container>
         <Heading>Single Garden</Heading>
         <Divider />
+        <Card>
+          <CardBody>
+          
+          <Button colorScheme='teal' variant='outline'>
+            Connect
+          </Button>
+          </CardBody>
+          <CardFooter>
+            <Button colorScheme='teal' variant='outline'>
+              Take me back to Garden listings
+            </Button>
+          </CardFooter>
+          </Card>
       </Container>
-      </ChakraProvider>
+    </ChakraProvider>
       </>
   );
 }
