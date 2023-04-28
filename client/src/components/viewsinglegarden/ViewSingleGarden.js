1ed205b738855af6b3a87dbe27a24c712c8f278b
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-04-28 11:52:55 +0100

    Basic page setup for a single garden

---
+++ client/src/components/viewsinglegarden/ViewSingleGarden.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { ChakraProvider, Container, Button, Heading, Divider, Input, Select } from "@chakra-ui/react";
+
+const ViewSingleGarden = () => {
+  return (
+    <>
+    <ChakraProvider>
+      <Container>
+        <Heading>Single Garden</Heading>
+        <Divider />
+      </Container>
+      </ChakraProvider>
+      </>
+  );
+}
+
