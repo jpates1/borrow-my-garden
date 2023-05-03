a11417f0e75a3718650889cea1f60949caf1e9bc
KumarS14 <sonjaykumar1404@gmail.com>
2023-05-03 13:21:14 +0100

    formatting

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const Login = ({ navigate }) => {
+const Login = ({navigate}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
