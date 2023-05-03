bc97155205b2a883dc7b8a35f4229ac799a366d6
Leah Massey <15648177+leah-massey@users.noreply.github.com>
2023-05-03 15:46:43 +0100

    navbar set up to NOT display on login or signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import NavBar from "./navbar";
+import NavBar from "./Navbar";
 import SignUp from "./components/signup/SignUp";
 import Login from "./components/auth/Login";
 import LandingPage from "./components/landingpage/LandingPage";
 import CreateGarden from "./components/createGarden/CreateGarden";
 import AllGardens from "./components/allgardens/AllGardens";
 import ViewSingleGarden from "./components/viewsinglegarden/ViewSingleGarden";
+import IsNavBarNeeded from "./components/isnavbarneeded/IsNavBarNeeded";
+
  
 const App = () => {
     const navigate = useNavigate();
 
  return (
    <div>
-     <NavBar />
+    <IsNavBarNeeded>
+      <NavBar />
+    </IsNavBarNeeded> 
      <Routes>
        <Route path="/signup" element={<SignUp navigate={navigate} />} />
        <Route path="/login" element={<Login navigate={navigate} />} />
