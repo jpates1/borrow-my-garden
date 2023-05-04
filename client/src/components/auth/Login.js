10d35530cd2600bf47812dae17ddf7379aaf54fb
Leah Massey <15648177+leah-massey@users.noreply.github.com>
2023-05-04 11:32:53 +0100

    login card layout formatting

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { ChakraProvider, 
-  Container, 
-  Button, 
-  Heading, 
+import {
+  Flex,
+  Box,
   FormControl,
-  Input,
   FormLabel,
-  Stack, 
-  Card, 
-  CardBody,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
   Text,
-  Image } from "@chakra-ui/react";
+  useColorModeValue,
+  VStack,
+  Card,
+  HStack,
+  CardBody,
+  Center
+} from '@chakra-ui/react';
+import Logo from "./logo-login.png";
 
 const Login = ({ navigate }) => {
   const [email, setEmail] = useState("");
@@ -45,41 +53,98 @@ const Login = ({ navigate }) => {
   }
 
   return (
-    <>
-      <ChakraProvider>
-        <Container>
-                    <Heading color='teal' paddingBottom='100px' align='center'>Borrow My Garden</Heading>
-          <Heading color='teal' paddingBottom='50px' align='center' size='md'>Login Page</Heading>
-  
-      <form onSubmit={handleSubmit}>
-        <FormLabel color='teal'>Email</FormLabel>
-        <Input placeholder='Email' id="email" type='text' value={ email } onChange={handleEmailChange} />
-        <FormLabel color='teal'>Password</FormLabel>
-        <Input placeholder='Password' id="password" type='password' value={ password } onChange={handlePasswordChange} />
-        <Button marginTop='10px' colorScheme="teal" variant="outline" role='submit-button' id='submit' type="submit" value="Submit">Sign In</Button>
-      </form>
+    
+    <Box pt="100">
+      <Center>
+      <Stack spacing="20">
+      <VStack as="header" spacing="6" mt="8">
+      <img 
+        src={Logo} 
+        alt='Logo' 
+        width={150} 
+        height={95} />
+      </VStack>
+      <Card bg="#f6f8fa" variant="outline" borderColor="#d8dee4" maxW="308px" pt="20" pb="20">
+        <CardBody pr="15" pl="15">
+          <form onSubmit={handleSubmit}>
+            <Stack spacing="15">
+              <FormControl >
+                
+                <FormLabel size="sm" color='teal' pb="8">Email</FormLabel>
+                
+                <Input 
+                  type='text' 
+                  placeholder='Email' 
+                  bg="white" 
+                  borderColor="#d8dee4" 
+                  size="sm" 
+                  borderRadius="6px" 
+                  id="email"  
+                  value={ email } 
+                  onChange={handleEmailChange} 
+                />
+                
+              </FormControl>
+              <FormControl>
+                
+                  <FormLabel size="sm" color="teal" pb="8">Password</FormLabel>
+                  <Input 
+                    placeholder='Password' 
+                    bg="white" 
+                    borderColor="#d8dee4" 
+                    size="sm" 
+                    borderRadius="6px" 
+                    id="password" 
+                    type='password' 
+                    value={ password } 
+                    onChange={handlePasswordChange} 
+                  />
+                
+              </FormControl>
+            
+              <Button 
+                bg="#2da44e" 
+                color="white" 
+                size="sm" 
+                borderRadius="6px"
+                _hover={{ bg: "#2c974b" }} 
+                _active={{bg: "#298e46"}} 
+                marginTop='10px' 
+                 
+                variant="solid" 
+                role='submit-button' 
+                id='submit' type="submit" 
+                value="Submit"
+              >
+              Sign In
+              </Button>
 
-    </Container>
-    </ChakraProvider>
-    </>
+            </Stack>
+          </form>
+        </CardBody>
+      </Card>
+
+      <Card variant='outline' borderColor='#d0d7de'>
+            <CardBody>
+              <Center>
+                <HStack fontSize='sm' spacing='1'>
+                  <Text>New here? </Text>
+                  <Link isExternal color='#0969da' href='#'>
+                    Create an account.
+                  </Link>
+                </HStack>
+              </Center>
+            </CardBody>
+        </Card>
+
+
+
+
+
+      </Stack>
+      </Center>
+    </Box>
   );
-
-
-
-
-
-
-
-  // return (
-  //   <div>
-  //     <h1>Login Page</h1>
-  //     <form onSubmit={handleSubmit}>
-  //       <input placeholder='Email' id="email" type='text' value={ email } onChange={handleEmailChange} />
-  //       <input placeholder='Password' id="password" type='password' value={ password } onChange={handlePasswordChange} />
-  //       <button role='submit-button' id='submit' type="submit" value="Submit">Sign In</button>
-  //     </form>
-  //   </div>
-  // );
 }
 
 export default Login;
