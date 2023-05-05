568249bc0a6a25feeacc836c5322530b60db59cd
jpates1 <jamesandrewpates@gmail.com>
2023-05-05 09:31:08 +0100

    add images to gardens

--- client/src/components/garden/Garden.js
+++ client/src/components/garden/Garden.js
@@ -9,6 +9,7 @@ import {
   UnorderedList,
   ListItem,
   Text,
+  Image,
 } from "@chakra-ui/react";
 import { Link as ReactRouterLink } from 'react-router-dom';
 
@@ -36,6 +37,7 @@ const Garden = ({ garden, detailed }) => {
                 <Button margin='15px' colorScheme="teal" variant="outline">Connect</Button>
               </>
             )}
+            <Image src='https://www.stevedimmockgardens.co.uk/wp-content/uploads/2018/06/B13.jpg' alt='Find a garden' />
           </CardBody>
           {detailed ? null : (
               <Link as={ReactRouterLink} to={`/gardens/${garden._id}`}>
